531c642a680104e2c74409b2296a240d0a903508
fatihbatu <77248966+fatihbatu@users.noreply.github.com>
2024-03-26 01:19:16 +0300

    setup: variant, verison and seed resources  create

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { VariantsModule } from './variants/variants.module';
+import { VersionsModule } from './versions/versions.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/scraper')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/scraper'),
+    VariantsModule,
+    VersionsModule,
+    SeedModule,
+  ],
   controllers: [],
   providers: [],
 })
