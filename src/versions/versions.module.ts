8ba397e5b13ee265366daa44bd98a4cab66c4d0e
fatihbatu <77248966+fatihbatu@users.noreply.github.com>
2024-03-26 13:38:44 +0300

    refactor: async and populate update

--- src/versions/versions.module.ts
+++ src/versions/versions.module.ts
@@ -3,10 +3,14 @@ import { VersionsService } from './versions.service';
 import { VersionsController } from './versions.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Version, VersionSchema } from 'src/schemas/Version.schema';
+import { Variant, VariantSchema } from 'src/schemas/Variant.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Version.name, schema: VersionSchema }]),
+    MongooseModule.forFeature([
+      { name: Version.name, schema: VersionSchema },
+      { name: Variant.name, schema: VariantSchema },
+    ]),
   ],
   controllers: [VersionsController],
   providers: [VersionsService],
--- src/versions/versions.service.ts
+++ src/versions/versions.service.ts
@@ -3,35 +3,50 @@ import { UpdateVersionDto } from './dto/update-version.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Version } from 'src/schemas/Version.schema';
 import { Model } from 'mongoose';
+import { Variant } from 'src/schemas/Variant.schema';
 
 @Injectable()
 export class VersionsService {
   constructor(
     @InjectModel(Version.name) private versionModel: Model<Version>,
+    @InjectModel(Variant.name) private variantModel: Model<Variant>,
   ) {}
 
-  findAll(): Promise<Version[]> {
-    const versions = this.versionModel.find().exec();
+  async findAll(): Promise<Version[]> {
+    const versions = await this.versionModel.find().exec();
     if (!versions) throw new NotFoundException('No versions found');
     return versions;
   }
 
-  findOne(id: string): Promise<Version> {
-    const version = this.versionModel.findById(id).exec();
+  async findOne(id: string): Promise<Version> {
+    const version = await this.versionModel
+      .findById(id)
+      .populate({
+        path: 'variants',
+        model: this.variantModel,
+        strictPopulate: false,
+      })
+      .exec();
     if (!version) throw new NotFoundException(`Version #${id} not found`);
     return version;
   }
 
-  update(id: string, updateVersionDto: UpdateVersionDto) {
-    const version = this.versionModel
+  async update(id: string, updateVersionDto: UpdateVersionDto) {
+    const version = await this.versionModel
       .findByIdAndUpdate(id, updateVersionDto, { new: true })
       .exec();
     if (!version) throw new NotFoundException(`Version #${id} not found`);
     return version;
   }
 
-  remove(id: string) {
-    const version = this.versionModel.findByIdAndDelete(id).exec();
+  async remove(id: string) {
+    const version = await this.versionModel
+      .findByIdAndDelete(id)
+      .exec()
+      .then(async (version) => {
+        await this.variantModel.deleteMany({ versionId: id }).exec();
+        return version;
+      });
     if (!version) throw new NotFoundException(`Version #${id} not found`);
     return version;
   }
