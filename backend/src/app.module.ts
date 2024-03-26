12a8192c2515aa8c3f2ec333461601ef7c94a2fc
fatihbatu <77248966+fatihbatu@users.noreply.github.com>
2024-03-27 00:49:26 +0300

    refactor: db connection method update to environment

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,18 @@ import { VariantsModule } from './variants/variants.module';
 import { VersionsModule } from './versions/versions.module';
 import { SeedModule } from './seed/seed.module';
 import { ValidateMiddleware } from './common/validate.middleware';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/scraper'),
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.DATABASE_URI, {
+      dbName: process.env.DATABASE_NAME,
+      auth: {
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASS,
+      },
+    }),
     VariantsModule,
     VersionsModule,
     SeedModule,
